74991a58393fa56bfa9616cd00944423c0aa7b70
Janderson Lima <backupaw197@gmail.com>
2023-11-28 21:23:56 -0300

    Finalização técnica da seção de nóticias rápidas

--- index.js
+++ index.js
@@ -11,8 +11,40 @@ const sub = document.getElementById('subtitulo');
 const ArraySubs = ['- Hoje é o dia 3 -', '- um dois, feijão com arroz -', '- Ok, está funcionando (^◡^ ) -'];
 let contador = 0;
 
-const fotos = document.getElementById('fotos')
+const fotos = document.getElementById('fotos');
+let idFoto = document.getElementById('clara')
+const tituloFast = document.getElementById('titulo-fast');
+const paragrafoFast = document.getElementById('paragrafo-fast');
+const btProx = document.getElementById('prox'); 
+const btAnt = document.getElementById('ant');
+let contFast = 1; 
+const fastArrayShort = [
+  { titulo: 'Clara, A Garota do Sol Poente', imagem: 'fotos/clara.png' },
+  { titulo: "Alonne, Voce não precisa se sentir só", imagem: 'fotos/alonne.jpeg' }
+];
 
+const fastArrayLong = [{paragrafo: 'Clara, é uma garota que costuma sair a tarde para que o sol\n de 40 graus não machuque sua pele.'},
+{paragrafo: 'Fiz o que queria fazer, mas nao disse o que queria dizer.\n No fim, vc só me viu como um estorvo.'}]
+
+btProx.addEventListener('click', () => {
+  tituloFast.innerText = fastArrayShort[contFast].titulo;
+  fotos.querySelector('img').src = fastArrayShort[contFast].imagem;
+  paragrafoFast.innerText = fastArrayLong[contFast].paragrafo;
+  contFast++;
+  if (contFast >= fastArrayShort.length) {
+    contFast = 0; 
+  }
+});
+
+btAnt.addEventListener('click', () => {
+    tituloFast.innerText = fastArrayShort[contFast].titulo;
+    fotos.querySelector('img').src = fastArrayShort[contFast].imagem;
+    paragrafoFast.innerText = fastArrayLong[contFast].paragrafo;
+    contFast--;
+    if (contFast < 0) {
+      contFast = (fastArrayShort.length - 1); 
+    }
+})
 setInterval(() => {
     sub.innerText = ArraySubs[contador];
     contador++;
