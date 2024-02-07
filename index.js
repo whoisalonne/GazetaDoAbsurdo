63a729fceb7c8e848f634928cac1f8daf3526124
Janderson Lima <backupaw197@gmail.com>
2024-02-07 00:41:24 -0300

    Atualização 2.65 - todas as seções de notícias melhoras, add do cabecalho sem funcionamento, add da seção de 'outras not' e exclusão da aba'fast notices'

--- index.js
+++ index.js
@@ -11,51 +11,11 @@ const sub = document.getElementById('subtitulo');
 const ArraySubs = ['- O contéudo deste site não é verídico e não deve ser levado a sério de forma alguma -', '- Gazeta do Povo? Nunca ouvi falar -', '- Comentários em breve (^◡^ ) -', 'Versão mobile ainda não disponivel'];
 let contador = 0;
 
-const fotos = document.getElementById('fotos-fast');
-let idFoto = document.getElementById('clara');
-const tituloFast = document.getElementById('t-fast');
-const paragrafoFast = document.getElementById('p-fast');
-const btProx = document.getElementById('prox');
-const btAnt = document.getElementById('ant');
-let contFast = 1;
-const fastArrayShort = [
-    { titulo: 'Clara, A Garota do Sol Poente', imagem: 'fotos/clara.png' }
-];
-
-const fastArrayLong = [{ paragrafo: 'Jovem estudante do interior do Ceará alega sofrer com temperaturas acima dos 33 graus, visando evitar o calor intenso da sua cidade, busca sair somente à tarde para proteger sua pele. Quando nossa equipe entrou em contato para uma entrevista sobre como ela consegue frequentar suas aulas no turno matutino obtivemos a breve resposta "Eu pego o onibús, né?"' }]
-
-btProx.addEventListener('click', () => {
-    tituloFast.innerText = fastArrayShort[contFast].titulo;
-    fotos.querySelector('img').src = fastArrayShort[contFast].imagem;
-    paragrafoFast.innerText = fastArrayLong[contFast].paragrafo;
-    contFast++;
-    if (contFast >= fastArrayShort.length) {
-        contFast = 0;
-    }
-});
-
-btAnt.addEventListener('click', () => {
-    tituloFast.innerText = fastArrayShort[contFast].titulo;
-    fotos.querySelector('img').src = fastArrayShort[contFast].imagem;
-    paragrafoFast.innerText = fastArrayLong[contFast].paragrafo;
-    contFast--;
-    if (contFast < 0) {
-        contFast = (fastArrayShort.length - 1);
-    }
-})
 setInterval(() => {
     sub.innerText = ArraySubs[contador];
     contador++;
     if (contador === 4) { contador = 0 };
 }, 10000)
-
-fotos.addEventListener('click', () => {
-    if (fotos.style.transform === 'translateX(120%)') {
-        fotos.style.transform = 'translateX(0%)';
-    } else {
-        fotos.style.transform = 'translateX(120%)';
-    }
-})
 
 anoA.innerText = ano;
 diaA.innerText = dia;
@@ -98,4 +58,3 @@ switch (mes) {
         mesA.innerText = "Dez";
         break;
 }
-
